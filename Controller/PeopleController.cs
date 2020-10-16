40bd908ee7f3750e4c778d1bf56d252dd052be4a
Alyson Tiago S. Ferreira <43752173+alyson-ferreira@users.noreply.github.com>
2020-10-16 09:37:37 -0300

    first commit

---
+++ Controller/PeopleController.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using Exemplo.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Exemplo.Controller
+
+{
+    [Route ("[controller]")]
+    public class PeopleController {
+        public List<People> peoples = new List<People> ();
+
+        [HttpGet]
+        public List<People> Get () { return peoples; }
+
+        [HttpPost]
+        public List<People> Post ([FromBody] People people) {
+            peoples.Add (people);
+            return peoples;
+        }
+
+        [HttpPut]
+        public List<People> Put ([FromBody] People people) {
+            var index = peoples.FindIndex (x => x.id == people.id);
+            if (index == -1) {
+                return peoples;
+            }
+            peoples[index] = people;
+            return peoples;
+
+        }
+
+    
+
+        [HttpDelete]
+        public List<People> Delete ([FromBody] People people)
+
+    }
+
+}
